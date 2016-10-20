941ef21be527319dcec852848775be9e222b29b9
dfresh613 <douglas.rohde2@gmail.com>
2016-10-19 20:07:14 -0400

    make root route to messagereceiver

--- strangerLights/urls.py
+++ strangerLights/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^messagereceiver/', include('messagereceiver.urls')),
+    url(r'^$', lambda r: HttpResponseRedirect('messagereceiver/')),
 
 ]
