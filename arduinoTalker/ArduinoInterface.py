66c9f2d89736b5622b908a606018044b2f2dbd15
drohde <drohde@sciencelogic.com>
2016-10-23 20:28:32 -0400

    unit test fixes

--- arduinoTalker/ArduinoInterface.py
+++ arduinoTalker/ArduinoInterface.py
@@ -3,7 +3,10 @@ import serial
 class ArduinoInterface:
     def __init__(self):
         # TODO: modify to use actual id
-        ser=serial.Serial('/dev/ttyACM0', 9600)
+        self.ser=serial.Serial('/dev/ttyACM0', 9600)
 
     def push_message(self, message):
         self.ser.write(message)
+
+    def read_message(self):
+        return self.ser.readLine();
